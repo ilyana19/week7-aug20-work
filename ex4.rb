15e55648fe226156927012883c87d246cfd8f407
ilyana19 <aries_2152@hotmail.com>
2018-08-24 10:38:22 -0400

    made a minor if statement priority change

--- ex4.rb
+++ ex4.rb
@@ -1,12 +1,12 @@
 array = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
 
 array.each do |word|
-  if word == word.downcase
+  if word.length > 4 && word == word.downcase
+    puts "long and lowercase"
+  elsif word == word.downcase
     puts "lowercase"
   elsif word.length > 4
     puts "long"
-  elsif word.length > 4 && word == word.downcase
-    puts "long and lowercase"
   else
     puts word
   end
